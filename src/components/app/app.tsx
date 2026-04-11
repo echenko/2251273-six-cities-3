5f6d8565a24462c93d04328f631a8136c5690f69
Echenko <ehenko.v.g@yandex.ru>
2026-04-11 18:16:52 +0300

    fix: component app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,11 @@
 // Import Components Pages
 // TODO: Исправить!
 import MainPage from '../../pages/main-page';
-import OfferPage from '../../pages/offer-page';
+// import OfferPage from '../../pages/offer-page';
 // Import Types
 import { OffersElementType } from '../../mocks/offers-mocks';
-import { OFFER } from '../../mocks/offer-mock';
-import { COMMENTS } from '../../mocks/comments-mocks';
+// import { OFFER } from '../../mocks/offer-mock';
+// import { COMMENTS } from '../../mocks/comments-mocks';
 
 // Create App
 function App({offers}: {offers: OffersElementType[]}): JSX.Element {
@@ -15,11 +15,11 @@ function App({offers}: {offers: OffersElementType[]}): JSX.Element {
       <MainPage
         offers={offers}
       />
-      <OfferPage
+      {/* <OfferPage
         offers={offers}
         offer={OFFER}
         comments={COMMENTS}
-      />
+      /> */}
     </>
   );
 }
