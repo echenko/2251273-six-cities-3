4db6d6cc9ecd503a0b1587f177479260b26f9534
Echenko <ehenko.v.g@yandex.ru>
2026-04-18 12:24:18 +0300

    fix: twitching layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,8 @@
 // Import React
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+// Import Styles
+import { createGlobalStyle } from 'styled-components';
 // Import Components Pages
 import { MainPage } from '../../pages/main-page';
 import { OfferPage } from '../../pages/offer-page';
@@ -18,11 +20,26 @@ import { OffersElementType, OFFERS } from '../../mocks/offers-mocks';
 import { COMMENTS } from '../../mocks/comments-mocks';
 import { FAVORITES } from '../../mocks/favorite-mocks';
 
+// Create GlobalStyle
+// TODO: Create GlobalStyle!
+export const GlobalStyle = createGlobalStyle`
+  body,
+  html {
+    width: auto;
+    margin: 0;
+  }
+
+  html {
+    margin-left: calc(100vw - 100%);
+  }
+  `;
+
 // Create App
 function App(): JSX.Element {
   const offers: OffersElementType[] = OFFERS;
   return (
     <HelmetProvider>
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route
