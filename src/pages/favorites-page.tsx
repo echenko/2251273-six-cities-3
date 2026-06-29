ec475a44b058663bde6adedfa5e5a0966f84015f
Echenko <ehenko.v.g@yandex.ru>
2026-06-29 11:08:20 +0300

    add: favorite empty and favorite test

--- src/pages/favorites-page.tsx
+++ src/pages/favorites-page.tsx
@@ -1,10 +1,21 @@
 import { Favorites } from '../components/favorites/favorites';
+import { FavoritesEmpty } from '../components/favorites/favorites-empty';
+import { useAppSelector } from '../hooks/hooks';
+import { getFavoriteOffers } from '../store/selectors/offers-slice';
+import { clsx } from 'clsx';
 
 function FavoritesPage(): JSX.Element {
+  const favoritesOffers = useAppSelector(getFavoriteOffers);
+
   return (
     <main className="page__main page__main--favorites">
-      <div className="page__favorites-container container">
-        <Favorites />
+      <div className={
+        clsx('page__favorites-container container',
+          { 'page__favorites-container--empty': !favoritesOffers.length })
+      }
+      >
+        {!!favoritesOffers.length && <Favorites />}
+        {!favoritesOffers.length && <FavoritesEmpty />}
       </div>
     </main>
   );
